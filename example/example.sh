da11e534976d060ae6ef8962f208e5c86e0f24a8
github1 <john.collier4@gmail.com>
2019-08-14 00:23:49 -0400

    update example

--- example/example.sh
+++ example/example.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+CLUSTER_NODE=$(docker ps | grep 'example_node-member' | awk '{print $1}')
+
+if [[ -z "${CLUSTER_NODE}" ]]; then
+  echo "cluster node not found"
+  exit 0
+fi
+
 docker exec \
-    -it $(docker ps | grep node-a | awk '{print $1}') \
+    -it ${CLUSTER_NODE} \
     curl http://localhost:8080/api/echo/$RANDOM | jq .
